a2550810a3ff189b5ed983738e7e7736590a3100
Israel Mendoza <xander_irsl@hotmail.com>
2020-09-27 14:32:18 -0500

    Formatted first project code

--- Constants/Constants and bitwise ops/main.go
+++ Constants/Constants and bitwise ops/main.go
@@ -15,20 +15,29 @@ const (
 
 func main() {
 	// Setting persmissions using OR
-	var israRoles = emailAccess | accessVPN | laptop | adminRights | homeOffice | traveler // 1011 1101
+	var israRoles = emailAccess | accessVPN | laptop |
+		adminRights | homeOffice | traveler // 1011 1101
 
 	// Printing permissions
 	fmt.Printf("Isra's permissions: %b\n\n", israRoles) // 1011 1001
 
 	// Testing permissions using AND
-	fmt.Printf("Does Isra have an email address? %v\n", yesNo(israRoles&emailAccess == emailAccess))    // Yes
-	fmt.Printf("Does Isra have an O365 licence? %v\n", yesNo(israRoles&office365 == office365))         // No
-	fmt.Printf("Does Isra have VPN access? %v\n", yesNo(israRoles&accessVPN == accessVPN))              // Yes
-	fmt.Printf("Does Isra have a compnay laptop? %v\n", yesNo(israRoles&laptop == laptop))              // Yes
-	fmt.Printf("Does Isra have a admin credentials? %v\n", yesNo(israRoles&adminRights == adminRights)) // Yes
-	fmt.Printf("Can Isra work from home? %v\n", yesNo(israRoles&homeOffice == homeOffice))              // Yes
-	fmt.Printf("Does Isra have payroll access? %v\n", yesNo(israRoles&payrollAccess == payrollAccess))  // No
-	fmt.Printf("Does Isra travel for work? %v\n", yesNo(israRoles&traveler == traveler))                // Yes
+	fmt.Printf("Does Isra have an email address? %v\n",
+		yesNo(israRoles&emailAccess == emailAccess)) // Yes
+	fmt.Printf("Does Isra have an O365 licence? %v\n",
+		yesNo(israRoles&office365 == office365)) // No
+	fmt.Printf("Does Isra have VPN access? %v\n",
+		yesNo(israRoles&accessVPN == accessVPN)) // Yes
+	fmt.Printf("Does Isra have a compnay laptop? %v\n",
+		yesNo(israRoles&laptop == laptop)) // Yes
+	fmt.Printf("Does Isra have a admin credentials? %v\n",
+		yesNo(israRoles&adminRights == adminRights)) // Yes
+	fmt.Printf("Can Isra work from home? %v\n",
+		yesNo(israRoles&homeOffice == homeOffice)) // Yes
+	fmt.Printf("Does Isra have payroll access? %v\n",
+		yesNo(israRoles&payrollAccess == payrollAccess)) // No
+	fmt.Printf("Does Isra travel for work? %v\n",
+		yesNo(israRoles&traveler == traveler)) // Yes
 }
 
 func yesNo(b bool) string {
